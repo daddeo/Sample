85793d82b5043adcac1e68e5b05cc200385ba634
daddeo <jason.wallenfang@gmail.com>
2020-03-18 13:14:10 -0500

    changed code

--- test.py
+++ test.py
@@ -1,18 +1,9 @@
 import sys
-
 import requests
 
 print(sys.version)
 print(sys.executable)
 
-
-def greet(who):
-    greeting = "Hello, {}".format(who)
-    return greeting
-
-
-print(greet("World"))
-print(greet("Jason"))
-
 r = requests.get("http://www.google.com")
 print("status ", r.status_code)
+print("ok     ", r.ok)
